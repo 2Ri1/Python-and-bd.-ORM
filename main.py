dce9d15b6f1298e7c668c20115a78c0304d96744
Marina Shtareva <marina.siar@email.ru>
2023-04-13 22:48:04 +0300

    change_2

--- main.py
+++ main.py
@@ -7,19 +7,13 @@ from models import create_tables, Publisher, Shop, Book, Stock, Sale
 
 if __name__ == '__main__':
 	
-	DSN = "postgresql://postgres:kubik2103@localhost:5432/hw"
+	DSN = "postgresql://postgres:parol@localhost:5432/hw"
 	engine = sqlalchemy.create_engine(DSN)
 	create_tables(engine)
 	
 	Session = sessionmaker(bind=engine)
 	session = Session()
-	
-	# publisher = Publisher()
-	# shop = Shop()
-	# book = Book()
-	# stock = Stock()
-	# sale = Sale()
-	
+		
 	with open('fixtures/tests_data.json', 'r') as fd:
 		data = json.load(fd)
 		
@@ -39,20 +33,20 @@ if __name__ == '__main__':
 	
 	if your_request == 1:
 		your_request_1 = Publisher.name == input('Введите имя автора:')
-	q = session.query(Book.title, Shop.name, Sale.price, Sale.count, Sale.date_sale).\
-    join(Publisher).join(Stock).join(Sale).join(Shop).\
-	filter(your_request_1).order_by(Sale.date_sale)
-	print(q)
-	for book, shop, price, count, date in q:
-		print("\t"f'{book:<40} | {shop:<10} | {price*count:<8} | {date}')
+		q = session.query(Book.title, Shop.name, Sale.price, Sale.count, Sale.date_sale).\
+			        join(Publisher).join(Stock).join(Sale).join(Shop).\
+								filter(your_request_1).order_by(Sale.date_sale)
+		print(q)
+		for book, shop, price, count, date in q:
+			print("\t"f'{book:<40} | {shop:<10} | {price*count:<8} | {date}')
 	
 	else:
 		your_request_2 = Publisher.id == input('Введите id автора:')
-	q = session.query(Book.title, Shop.name, Sale.price, Sale.count, Sale.date_sale).\
-    join(Publisher).join(Stock).join(Sale).join(Shop).\
-	filter(your_request_1).order_by(Sale.date_sale)
-	print(q)
-	for book, shop, price, count, date in q:
-		print("\t"f'{book:<40} | {shop:<10} | {price*count:<8} | {date}')
+		q = session.query(Book.title, Shop.name, Sale.price, Sale.count, Sale.date_sale).\
+			        join(Publisher).join(Stock).join(Sale).join(Shop).\
+								filter(your_request_2).order_by(Sale.date_sale)
+		print(q)
+		for book, shop, price, count, date in q:
+			print("\t"f'{book:<40} | {shop:<10} | {price*count:<8} | {date}')
 			
 	session.commit()
